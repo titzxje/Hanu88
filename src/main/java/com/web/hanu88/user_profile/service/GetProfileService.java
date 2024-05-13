d0b7ea958a45035dd5b4e92cc1fed1cbd889f70a
Tittx <datnt@pando-infinity.com>
2024-05-13 23:00:07 +0700

    implement get user profile

--- src/main/java/com/web/hanu88/user_profile/service/GetProfileService.java
+++ src/main/java/com/web/hanu88/user_profile/service/GetProfileService.java
@@ -1,7 +1,11 @@
 package com.web.hanu88.user_profile.service;
 
+import com.web.hanu88.share.entity.Result;
+import com.web.hanu88.user_profile.model.UserProfile;
 import com.web.hanu88.user_profile.repository.UserProfileRepository;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +13,36 @@ import org.springframework.stereotype.Service;
 public class GetProfileService {
     private final UserProfileRepository userProfileRepository;
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Input {
+        public long accountId;
+    }
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Output {
+        private String email;
+        private String displayName;
+        private long balancePoint;
+        private String avatarUrl;
+        private long roundPlayed;
+    }
+
+    public Result<?> getUserProfile(Input input) {
+        UserProfile userProfile = this.userProfileRepository.findByAccountId(input.accountId);
+        if (userProfile == null) {
+            return Result.failed("Not found this profile");
+        }
+        return Result.success(new Output(
+                userProfile.getEmail(),
+                userProfile.getDisplayName(),
+                userProfile.getBalancePoint(),
+                userProfile.getAvatarUrl(),
+                userProfile.getRoundPlayed()
+                )
+        );
+    }
 }
