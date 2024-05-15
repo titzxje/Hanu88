4430467213aa9a6aafbb26c638557889140c2f68
toanducnguyen <ndtoan20122002@gmail.com>
2024-05-15 14:53:16 +0700

    api createOrder

---
+++ src/main/java/com/web/hanu88/market/model/Order.java
@@ -0,0 +1,30 @@
+package com.web.hanu88.market.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_profile")
+@Getter
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private String url;
+    private Effect effect;
+    private long pricePoint;
+    private String status;
+
+    public Order(String name, String description, String url, Effect effect, long pricePoint, String status) {
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.effect = effect;
+        this.pricePoint = pricePoint;
+        this.status = status;
+    }
+}
